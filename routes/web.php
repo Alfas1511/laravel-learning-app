2c66031b724bc93332e0a6728a5c30f05f59d9b3
Alfas Salim <alfas@meridian.net.in>
2025-02-11 10:40:51 +0530

    created new dashboard page, login page and added assets

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
 });
